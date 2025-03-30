58be2ecadcf2ea9ed5d6ae04d4183401c3e43e92
Saki Ota <saki@SakinoMacBook-Air.local>
2025-03-30 15:06:02 +1100

    dom manuplation practice

---
+++ week8/script.js
@@ -0,0 +1,17 @@
+function newsUpdate() {
+    const paragraph = document.getElementById("news");
+
+    // log to console - the browser console
+    console.log(paragraph);
+    console.log(paragraph.textContent);
+
+    // Grab the news
+    paragraph.textContent = "Here are the latest News headlines";
+}
+
+const listDiv = document.getElementById("list");
+console.log(listDiv);
+
+
+
+
