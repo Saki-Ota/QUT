06eedbad0e23f69460319737c0c2e3d3c424f9a2
Saki Ota <saki@SakinoMacBook-Air.local>
2025-06-28 16:37:45 +1000

    created login and register path

--- IFQ716/assignment_2/movie-app/routes/users.js
+++ IFQ716/assignment_2/movie-app/routes/users.js
@@ -1,9 +1,89 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
+router.post("/login", function (req, res, next) {
+  // 1. Retrieve email and password from req.body
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({
+      Error: true,
+      Message: "Request body incomplete, both email and password are required",
+    });
+  }
+  // 2. Determine if user already exists in table
+  const queryUser = req.db.from("users").select("*").where("email", email);
+  queryUser.then((users) => {
+    if (users.length === 0) {
+      console.log("User does not exist");
+      return res.status(401).json({
+        Error: true,
+        Message: "User does not exist",
+      });
+    }
+
+    // compare password with hash
+    const user = users[0];
+    return bcrypt.compare(password, user.password).then((match) => {
+      if (!match) {
+        console.log("Passwords do not match");
+        return res.status(401).json({
+          Error: true,
+          Message: "Incorrect password or email"
+        });
+      }
+
+      // create JWT token
+      const expires_in = 60 * 60 * 24; // 1 day
+      const exp = Math.floor(Date.now() / 1000) + expires_in;
+      const token = jwt.sign({ exp }, process.env.JWT_SECRET);
+
+      res.status(200).json({
+        token,
+        token_type: "Bearer",
+        expires_in,
+      });
+    });
+  });
+});
+
+router.post("/register", function (req, res, next) {
+  // 1. Retrieve email and password from req.body
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({
+      Error: true,
+      Message: "Request body incomplete, both email and password are required",
+    });
+  }
+  // 2. Determine if user already exists in table
+  const queryUsers = req.db.from("users").select("email").where("email", email);
+
+  // 2.1 If user exists, return error
+  queryUsers
+    .then((users) => {
+      if (users.length > 0) {
+        return res
+          .status(409)
+          .json({ Error: true, Message: "User already exists" });
+      }
+
+      // 2.2 If user does not exist, insert into table
+      const saltRounds = 10;
+      const hash = bcrypt.hashSync(password, saltRounds);
+      return req.db("users").insert({
+        email,
+        password: hash,
+      });
+    })
+    .then(() => {
+      res.status(201).json({ Error: false, Message: "User created" });
+    });
+});
 module.exports = router;
